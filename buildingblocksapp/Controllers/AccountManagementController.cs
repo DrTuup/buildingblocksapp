b2c2d9716212fa6207ca9fff95b2539342c9bda8
Brandon Wijnands <brandonwijnands@live.nl>
2023-06-15 14:45:16 +0200

    Basis opzet accountmanagement

---
+++ buildingblocksapp/Controllers/AccountManagementController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using buildingblocksapp.Models;
+using System.Web.Routing;
+using System.Web.Optimization;
+
+namespace buildingblocksapp.Controllers
+{
+    public class AccountManagementController : Controller
+    {
+        // GET: AccountManagement
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
