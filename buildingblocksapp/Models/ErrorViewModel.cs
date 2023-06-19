52de32fe4b907e33ed071a9ac974c729401fb773
Ruben Claessens <claes@MacBook-Pro.local>
2023-06-19 12:38:56 +0200

    create new project with .NET Core 7

---
+++ buildingblocksapp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace buildingblocksapp.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
